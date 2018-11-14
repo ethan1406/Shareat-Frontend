ca8cfb0067c94f4bb0ee528656739267585dcdeb
Ethan Chang <ethanc@usc.edu>
2018-11-14 03:23:54 -0800

    temp custom cell

--- SharEats/SharEats/CheckTableViewCell.swift
+++ SharEats/SharEats/CheckTableViewCell.swift
@@ -10,14 +10,51 @@ import UIKit
 
 class CheckTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var dishName: UILabel!
-    @IBOutlet weak var sharedByView: UIView!
+    var dishText : String?
+    
+    var dishName = UILabel()
+    var sharedByView = UIView()
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        dishName = UILabel()
+        sharedByView = UIView()
+        
+        dishName.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        dishName.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        dishName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        dishName.widthAnchor.constraint(equalToConstant: 150)
+        
+        contentView.addSubview(dishName)
+        
+        sharedByView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        sharedByView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        sharedByView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        sharedByView.leadingAnchor.constraint(equalTo: dishName.trailingAnchor).isActive = true
+        
+        contentView.addSubview(sharedByView)
+        //productImage.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nil, paddingTop: 5, paddingLeft: 5, paddingBottom: 5, paddingRight: 0, width: 90, height: 0, enableInsets: false)
 
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        if let text = dishText {
+            dishName.text = text
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
